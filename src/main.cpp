000fed3d6c8908b84bad4d7291cb85e9e4a6dadd
Rafid Zaman <rafidzaman@mac.lan>
2026-02-15 14:33:33 -0500

    initial

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <FEH.h>
+#include <Arduino.h>
+
+
+// Declare things like Motors, Servos, etc. here
+// For example:
+// FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
+// FEHServo servo(FEHServo::Servo0);
+
+
+void ERCMain()
+{
+    // Your code here!
+
+    // Or just use the TestGUI function
+    TestGUI();
+
+}
