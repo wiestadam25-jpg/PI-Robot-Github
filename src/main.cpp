be65d7c8e875ca83726915269d321d8b1ea62b6b
superxman1 <nickwickler1@gmail.com>
2026-02-15 14:16:45 -0500

    Added Comments

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,9 @@ void ERCMain()
     //Part 2
     while(!LCD.Touch(&x_position, &y_position))
     {
-        CdSvalue = CdS_cell.Value();
+        CdSvalue = CdS_cell.Value(); 
         cdSmin = .35;
-        cdsmax = 5;
+        cdsmax = 5; //Set values for min and max CdS values based on testing
         LCD.WriteLine(CdSvalue);
         degree = (CdSvalue - cdSmin) * 180 / (cdsmax - cdSmin); // calculate degree to set servo to based on CdS value
         servo.SetDegree(degree); // set servo degree based on CdS value
